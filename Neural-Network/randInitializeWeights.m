f3c4de2c5d1061c225cf0226684537d43b06b152
Aishwarya <as3907@columbia.edu>
2014-05-13 17:42:00 -0700

    function to initialize weights randomly

--- Neural-Network/randInitializeWeights.m
+++ Neural-Network/randInitializeWeights.m
@@ -1,10 +1,13 @@
 function W = randInitializeWeights(L_in, L_out)
+%L_in = number of units in input layer
+%L_out = number of units in output layer 
 
 W = zeros(L_out, 1 + L_in);
 
 % randomly choosing a small value
 epsilon_init = 0.12;
 
+% randomly initializes values between -epsilon and +epsilon
 W = rand(L_out, 1+L_in)*2*epsilon_init - epsilon_init;
 
 
