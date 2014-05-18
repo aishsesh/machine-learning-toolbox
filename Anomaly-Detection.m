12a24b06d7445a6d7cbf50773f7b9766c19f45e7
aishsesh <as3907@columbia.edu>
2014-05-18 16:43:08 -0700

    Anomaly Detection

---
+++ Anomaly-Detection.m
@@ -0,0 +1,57 @@
+
+
+
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+[m, n] = size(X);
+
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+mu = transpose(sum(X)/m);
+for i = 1:m,
+	sigma2 += (transpose(X(i,:)) - mu).^2;
+end;
+sigma2 = sigma2/m;
+	
+end
+
+
+
+
+
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval) using the F1 score.
+% the pval parameter is the probability density function of the multivariate gaussian distribution.
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+  predictions = (pval< epsilon);
+	tp = sum((yval == predictions) & (yval ==1));
+	fp = sum((yval != predictions) & (yval ==0));
+	fn = sum((yval != predictions) & (yval ==1));
+	prec = tp/ (tp + fp);
+	rec = tp/ (tp + fn);
+	F1 = 2* prec* rec / (prec+ rec);
+
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
